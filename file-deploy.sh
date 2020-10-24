e2590a9f2291cf63cad4440d3dca2e021ca31ee5
Willard <willardding@hotmail.com>
2020-10-24 09:45:44 +0000

    use the compiled play instead

--- file-deploy.sh
+++ file-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp ifi-streamer-tidal-connect.service /lib/systemd/system/
-chmod +x /usr/ifi/ifi-tidal/play.sh
+chmod +x /usr/ifi/ifi-tidal/play
 chmod +x /usr/ifi/ifi-tidal/pa_devs/run.sh
 #systemctl enable ifi-streamer-tidal-connect.service
